28beaf6b3f88a3bd74b216312582bcc148d0dcd0
antoniopresto <antoniopresto@gmail.com>
2019-08-30 09:37:09 -0300

    fix empty operationId

--- src/generate-client.ts
+++ src/generate-client.ts
@@ -343,7 +343,7 @@ const getTemplateView = (
         });
 
         let operationId = operation.operationId.replace("_", "");
-        if (tag) {
+        if (tag && tag.toLowerCase() !== operationId.toLowerCase()) {
           operationId = operationId.replace(tag, "");
         }
 
